cc74a8e96204228d6b1cf861d81ba405c6f8978c
Diego Pardo <diegojolin92@gmail.com>
2022-12-16 14:52:35 +0000

    Investigating parallel execution

--- src/rendering/gl_main.cpp
+++ src/rendering/gl_main.cpp
@@ -5,6 +5,9 @@
 
 #include <chrono>
 #include <exception>
+#include <thread>
+#include <atomic>
+#include <mutex>
 
 
 GLFWwindow *
@@ -52,25 +55,44 @@ render::
 run(GLFWwindow * const window)
 {
     Physics & physics{Physics::getSingleton()};
+    std::atomic<bool> refreshDraw{true};
+    std::mutex mtx;
+
+    auto advancePhysics =[&physics, &refreshDraw, &mtx](std::stop_token stoken)
+    {
+        while (!stoken.stop_requested())
+        {
+            std::lock_guard lck(mtx);
+            physics.advance();
+            refreshDraw = true;
+        }
+    };
+    std::jthread physicsThread(advancePhysics);
+
     TrianglesManager & tri_manager{physics.tri_manager};
 
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        physics.advance();
-
-        for (auto const & triangle_group : tri_manager.getTriangleGroups())
+        if (refreshDraw)
         {
-            render::drawTriangleGroup(triangle_group);
+            glClear(GL_COLOR_BUFFER_BIT);
+            std::lock_guard lck(mtx);
+            for (auto const & triangle_group : tri_manager.getTriangleGroups())
+            {
+                render::drawTriangleGroup(triangle_group);
+            }
+            render::drawBoundary();
+            refreshDraw = false;
         }
-
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
+    physicsThread.request_stop();
+    physicsThread.join();
+
     glfwTerminate();
 }
